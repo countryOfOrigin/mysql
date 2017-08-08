524c8ab645e72a1cba9bd87476162a226beba907
CG萝莉万岁 <2402347406@qq.com>
2017-08-08 14:08:29 +0800

    specialty

--- specialty.sql
+++ specialty.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-08-05 10:41:23
+Date: 2017-08-08 14:04:17
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,11 +24,19 @@ CREATE TABLE `classification` (
   `class_name` varchar(255) NOT NULL COMMENT '类别名称',
   PRIMARY KEY (`class_id`),
   KEY `class_name` (`class_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of classification
 -- ----------------------------
+INSERT INTO `classification` VALUES ('6', '养生');
+INSERT INTO `classification` VALUES ('1', '农副      ');
+INSERT INTO `classification` VALUES ('8', '居家');
+INSERT INTO `classification` VALUES ('2', '干货');
+INSERT INTO `classification` VALUES ('4', '水产');
+INSERT INTO `classification` VALUES ('7', '生鲜');
+INSERT INTO `classification` VALUES ('5', '酒茶');
+INSERT INTO `classification` VALUES ('3', '零食');
 
 -- ----------------------------
 -- Table structure for collection
@@ -38,7 +46,7 @@ CREATE TABLE `collection` (
   `coll_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL COMMENT '用户id',
   `good_id` int(11) NOT NULL COMMENT '商品id',
-  `time` datetime NOT NULL COMMENT '收藏时间',
+  `time` date NOT NULL COMMENT '收藏时间',
   PRIMARY KEY (`coll_id`),
   KEY `fk_collection_goods_good_id` (`good_id`),
   KEY `fk_collection_users_user_id` (`user_id`),
@@ -60,7 +68,7 @@ CREATE TABLE `comment` (
   `good_id` int(11) NOT NULL,
   `content` varchar(255) NOT NULL COMMENT '评论内容',
   `selection` int(11) NOT NULL COMMENT '评选项',
-  `time` datetime NOT NULL COMMENT '评论时间',
+  `time` date NOT NULL COMMENT '评论时间',
   PRIMARY KEY (`comm_id`),
   KEY `fk_comment_goods_good_id` (`good_id`),
   KEY `fk_comment_users_user_id` (`user_id`),
@@ -83,16 +91,30 @@ CREATE TABLE `goods` (
   `count` int(11) NOT NULL COMMENT '数量',
   `cost` decimal(10,0) NOT NULL COMMENT '成本',
   `sales_volume` int(11) NOT NULL COMMENT '销量',
-  `time` datetime NOT NULL COMMENT '上架时间',
+  `time` date NOT NULL COMMENT '上架时间',
   `parameter` varchar(255) NOT NULL COMMENT '产品参数',
   `recommend` tinyint(4) NOT NULL COMMENT '推荐商品标志',
   `url` varchar(255) NOT NULL COMMENT '图片路径',
   PRIMARY KEY (`good_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
+INSERT INTO `goods` VALUES ('1', '土豆', '23123', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/nongfu/01.jpg');
+INSERT INTO `goods` VALUES ('2', '瓜子', '2313', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/ganhuo/02.jpg');
+INSERT INTO `goods` VALUES ('3', '薯片', '1231', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/lingshi/03.jpg');
+INSERT INTO `goods` VALUES ('4', '鲜虾', '12', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shuichan/04.jpg');
+INSERT INTO `goods` VALUES ('5', '海带', '0', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shuichan/05.jpg');
+INSERT INTO `goods` VALUES ('6', '1111', '0', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shuichan/06.jpg');
+INSERT INTO `goods` VALUES ('7', '22222', '0', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shuichan/07.jpg');
+INSERT INTO `goods` VALUES ('8', '23123123', '0', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shuichan/08.jpg');
+INSERT INTO `goods` VALUES ('9', '谁打谁', '0', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shuichan/09.jpg');
+INSERT INTO `goods` VALUES ('10', '好了', '0', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shuichan/10.jpg');
+INSERT INTO `goods` VALUES ('11', '哈尔滨啤酒', '3', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/jiucha/11.jpg');
+INSERT INTO `goods` VALUES ('12', '人参', '20', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/yangsheng/12\r\n.jpg');
+INSERT INTO `goods` VALUES ('13', '苹果', '2', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/shengxian/13\r\n.jpg');
+INSERT INTO `goods` VALUES ('14', '居家', '20', '0', '0', '0', '0000-00-00', '', '0', '../../static/img/jujia/14\r\n.jpg');
 
 -- ----------------------------
 -- Table structure for goods_classification
@@ -107,11 +129,25 @@ CREATE TABLE `goods_classification` (
   KEY `fk_goods_classification_goods_good_id` (`good_id`),
   CONSTRAINT `fk_goods_classification_classification_class_name` FOREIGN KEY (`class_name`) REFERENCES `classification` (`class_name`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_goods_classification_goods_good_id` FOREIGN KEY (`good_id`) REFERENCES `goods` (`good_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of goods_classification
 -- ----------------------------
+INSERT INTO `goods_classification` VALUES ('1', '1', '农副');
+INSERT INTO `goods_classification` VALUES ('2', '4', '水产');
+INSERT INTO `goods_classification` VALUES ('3', '3', '零食');
+INSERT INTO `goods_classification` VALUES ('4', '2', '干货');
+INSERT INTO `goods_classification` VALUES ('5', '5', '水产');
+INSERT INTO `goods_classification` VALUES ('6', '6', '水产');
+INSERT INTO `goods_classification` VALUES ('15', '7', '水产');
+INSERT INTO `goods_classification` VALUES ('16', '8', '水产');
+INSERT INTO `goods_classification` VALUES ('17', '9', '水产');
+INSERT INTO `goods_classification` VALUES ('19', '10', '水产');
+INSERT INTO `goods_classification` VALUES ('20', '11', '酒茶');
+INSERT INTO `goods_classification` VALUES ('21', '12', '养生');
+INSERT INTO `goods_classification` VALUES ('22', '13', '生鲜');
+INSERT INTO `goods_classification` VALUES ('24', '14', '居家');
 
 -- ----------------------------
 -- Table structure for goods_receipt
@@ -138,7 +174,7 @@ CREATE TABLE `goods_receipt` (
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `time` datetime NOT NULL COMMENT '订单时间',
+  `time` date NOT NULL COMMENT '订单时间',
   `user_id` int(11) NOT NULL,
   `good_id` int(11) NOT NULL,
   `receipt_id` int(11) NOT NULL,
@@ -163,7 +199,7 @@ DROP TABLE IF EXISTS `recent_browse`;
 CREATE TABLE `recent_browse` (
   `good_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `time` datetime NOT NULL COMMENT '访问时间',
+  `time` date NOT NULL COMMENT '访问时间',
   PRIMARY KEY (`good_id`),
   KEY `fk_recent_browse_users_user_id` (`user_id`),
   CONSTRAINT `fk_recent_browse_goods_good_id` FOREIGN KEY (`good_id`) REFERENCES `goods` (`good_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -172,6 +208,24 @@ CREATE TABLE `recent_browse` (
 
 -- ----------------------------
 -- Records of recent_browse
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sales
+-- ----------------------------
+DROP TABLE IF EXISTS `sales`;
+CREATE TABLE `sales` (
+  `sale_id` int(11) NOT NULL AUTO_INCREMENT,
+  `good_id` int(11) NOT NULL,
+  `count` int(11) NOT NULL COMMENT '售出数量',
+  `time` date NOT NULL COMMENT '售出时间',
+  PRIMARY KEY (`sale_id`),
+  KEY `fk_sales_goods_good_id` (`good_id`),
+  CONSTRAINT `fk_sales_goods_good_id` FOREIGN KEY (`good_id`) REFERENCES `goods` (`good_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sales
 -- ----------------------------
 
 -- ----------------------------
@@ -202,15 +256,17 @@ CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
-  `time` datetime NOT NULL COMMENT '注册时间',
+  `time` date NOT NULL COMMENT '注册时间',
   `grade` int(11) NOT NULL COMMENT '等级',
   `email` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL COMMENT '收货默认地址',
   `telephone` varchar(255) NOT NULL COMMENT '绑定电话',
   `url` varchar(255) NOT NULL COMMENT '头像路径',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
+INSERT INTO `users` VALUES ('1', '123', 'asd', '0000-00-00', '0', '', '', '123', '');
+INSERT INTO `users` VALUES ('2', 'lehasdad', 'asdad1231', '0000-00-00', '0', '', '', '18454544545', '');
